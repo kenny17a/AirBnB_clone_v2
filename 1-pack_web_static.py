8bad1b51b15201c5c7238c931d21b04e85736f48
bolaji <ibrahimkenny17@gmail.com>
2022-09-13 14:36:39 -0700

    fixes

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,44 +1,30 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-import uuid
-from datetime import datetime
+"""
+    Generates a .tgz archive from contents of
+    the web_static folder of your AirBnB Clone
+    repo, using the function do_pack.
+    def do_pack():
+    All files must be added to the final archive
+    and stored in the folder "versions"
+    The name of the archive created must be
+    web_static_<year><month><day><hour><minute><second>.tgz
+    The function do_pack must return the archive
+    path if the archive has been correctly generated.
+    Otherwise, it should return None
+"""
+import datetime
+from fabric.api import local
+from os.path import isdir
 
 
-class BaseModel:
-    """A base class for all hbnb models"""
-    def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
-
-    def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.save()
-
-    def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+def do_pack():
+    """Compresses the web_static folder into a .tgz archive"""
+    try:
+        day = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_N = "versions/web_static_{}.tgz".format(day)
+        local("tar -czvf {} web_static".format(file_N))
+        return file_N
+    except FileNotFoundError:
+        return None
